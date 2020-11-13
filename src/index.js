d37bc1ed2d65c5d489693af95290a0d36aa9891b
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-11-13 15:19:35 -0500

    bootstrapping

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const root = document.getElementById("root");
+// eslint-disable-next-line react/jsx-filename-extension
+ReactDOM.render(<App />, root);
