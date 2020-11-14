b7cabeb0ab2ded95394cf6195007473119d25887
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-11-14 15:36:18 -0500

    point 4,5,6

--- src/app/pokemon/List/components/Item.jsx
+++ src/app/pokemon/List/components/Item.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 
 import "./Item.css";
+import { useHistory } from "react-router-dom";
 
 const ListItem = ({ id, name }) => {
+  const history = useHistory();
+
+  const goToDetails = useCallback(() => {
+    history.push(`/pokemon/${name}`);
+  }, [name]);
+
   return (
     <div className="pokemon-list-items">
       <div>
         <div className="img-placeholder" />
       </div>
-      <div className="id-and-name">
+      <div
+        className="id-and-name"
+        onClick={goToDetails}
+        tabIndex={0}
+        onKeyPress={goToDetails}
+        role="button"
+      >
         <div style={{ "padding-right": 20 }}>
           # <br />
           {String(id).padStart(3, "0")}
