33198561067c0e11cc51fcad00e675b925550f50
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-11-15 15:47:12 -0500

    list styling

--- src/app/pokemon/List/components/Item.jsx
+++ src/app/pokemon/List/components/Item.jsx
@@ -16,33 +16,32 @@ const ListItem = ({ name }) => {
 
   return (
     <div className="pokemon-list-items">
-      <div>
+      <div
+        onClick={goToDetails}
+        tabIndex={0}
+        onKeyPress={goToDetails}
+        role="button"
+      >
         {details.id ? (
           <div>
             <img
               width="180px"
               src={`https://pokeres.bastionbot.org/images/pokemon/${details.id}.png`}
               alt={details.name}
-              style={{ filter: "drop-shadow(black 2px 7px 10px)" }}
+              className="pokemon-portrait"
             />
           </div>
         ) : (
           <div className="img-placeholder">Loading</div>
         )}
       </div>
-      <div
-        className="id-and-name"
-        onClick={goToDetails}
-        tabIndex={0}
-        onKeyPress={goToDetails}
-        role="button"
-      >
+      <div className="id-and-name">
         <div style={{ "padding-right": 20 }}>
           # <br />
           {String(details.id).padStart(3, "0")}
         </div>
         <div style={{ wordBreak: "break-all" }}>
-          {details.name && titleCase(details.name)}
+          <h3>{details.name && titleCase(details.name)}</h3>
         </div>
       </div>
     </div>
