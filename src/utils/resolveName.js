5c8268f9672f716839f80410925cf74077f24afe
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-11-16 23:35:24 -0500

    fix edge case where names with hyphen

--- src/utils/resolveName.js
+++ src/utils/resolveName.js
@@ -1,5 +1,5 @@
 export default (name) => {
-  const strName = name.replace(/[^0-9a-zA-Z]/g, "");
+  const strName = name.replace(/[^\-0-9a-zA-Z]/g, "");
   if (/^[0-9]*$/.test(strName)) return strName;
 
   return strName.replace(/[0-9]/g, "").replace(" ", "-").toLowerCase();
