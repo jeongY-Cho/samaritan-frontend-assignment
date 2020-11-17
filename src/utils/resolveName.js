c69f98f3d19817a088a1d3465e95ed00be09b50d
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-11-16 20:39:51 -0500

    remove unnecessary log

--- src/utils/resolveName.js
+++ src/utils/resolveName.js
@@ -2,7 +2,5 @@ export default (name) => {
   const strName = name.replace(/[^0-9a-zA-Z]/g, "");
   if (/^[0-9]*$/.test(strName)) return strName;
 
-  const aa = strName.replace(/[0-9]/g, "").replace(" ", "-").toLowerCase();
-  console.log(aa);
-  return aa;
+  return strName.replace(/[0-9]/g, "").replace(" ", "-").toLowerCase();
 };
